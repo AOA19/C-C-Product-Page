97ed3bc48ddd75a5b9996007a6206bbb9b2f1674
A0A <66294921+AOA19@users.noreply.github.com>
2022-07-23 23:53:23 -0400

    Update mobileNavBar.js
    Updated JS code so the mobile navigation bar would be web accessible

--- mobileNavBar.js
+++ mobileNavBar.js
@@ -1,20 +1,23 @@
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
-const close = document.querySelector(".fas");
 
 // Hamburger toggle function:
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  close.classList.toggle("fa-times");
-  navMenu.classList.toggle("active");
+  const visible = navMenu.getAttribute("data-visible");
+  if (visible === "false") {
+    navMenu.setAttribute("data-visible", true);
+    hamburger.setAttribute("aria-expanded", true);
+  } else {
+    navMenu.setAttribute("data-visible", false);
+    hamburger.setAttribute("aria-expanded", false);
+  }
 });
 
 // Function to close the hamburger menu when the links are clicked:
 navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-  hamburger.classList.remove("active");
-  close.classList.remove("fa-times");
-  navMenu.classList.remove("active");
+  navMenu.setAttribute("data-visible", false);
+  hamburger.setAttribute("aria-expanded", false);
 }
